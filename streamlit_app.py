52cdfd6db6ee5e51671a2fc0803c9e0a0febde9b
bsunilse2021 <97008608+bsunilse2021@users.noreply.github.com>
2026-07-22 17:44:42 +0530

    Update streamlit_app.py
    St dataframe to pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,9 @@ st.write("Name on your Smoothie will be: ",)
 cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df=my_dataframe.topandas()
+st.dataframe(pd_df)
 st.stop()
 ingredients_list=st.multiselect(
     "Choose upto 5 ingredients:",
